f726fc019e7b3c9ac00bd678a9bb1da6f5dc1e2e
Alexandre Rouleau <alexandre.rleau@gmail.com>
2022-03-07 14:28:56 +0100

    création entitées client unique, entreprise, utilisateurclient, facture 2

--- AssuranceHardis/AssuranceHardis-ejb/src/java/Modele/UtilisateurClient.java
+++ AssuranceHardis/AssuranceHardis-ejb/src/java/Modele/UtilisateurClient.java
@@ -6,22 +6,112 @@
 package Modele;
 
 import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Temporal;
 
 /**
  *
  * @author alex_
  */
 @Entity
+
 public class UtilisateurClient implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    private Long id;  
+    @Column(nullable=false)
+    private String login;  
+    @Column(nullable=false)
+    private String mdp;
+    @Temporal(javax.persistence.TemporalType.DATE)
+    @Column(nullable=false)
+    private Date dateCreationUser; 
+    @Column(nullable=false)
+    private String typeUser;
+
+    /**
+     * Get the value of typeUser
+     *
+     * @return the value of typeUser
+     */
+    public String getTypeUser() {
+        return typeUser;
+    }
+
+    /**
+     * Set the value of typeUser
+     *
+     * @param typeUser new value of typeUser
+     */
+    public void setTypeUser(String typeUser) {
+        this.typeUser = typeUser;
+    }
+
+
+    /**
+     * Get the value of dateCreationUser
+     *
+     * @return the value of dateCreationUser
+     */
+    public Date getDateCreationUser() {
+        return dateCreationUser;
+    }
+
+    /**
+     * Set the value of dateCreationUser
+     *
+     * @param dateCreationUser new value of dateCreationUser
+     */
+    public void setDateCreationUser(Date dateCreationUser) {
+        this.dateCreationUser = dateCreationUser;
+    }
+
+
+    /**
+     * Get the value of mdp
+     *
+     * @return the value of mdp
+     */
+    public String getMdp() {
+        return mdp;
+    }
+
+    /**
+     * Set the value of mdp
+     *
+     * @param mdp new value of mdp
+     */
+    public void setMdp(String mdp) {
+        this.mdp = mdp;
+    }
+
+
+    /**
+     * Get the value of login
+     *
+     * @return the value of login
+     */
+    public String getLogin() {
+        return login;
+    }
+
+    /**
+     * Set the value of login
+     *
+     * @param login new value of login
+     */
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    
 
     public Long getId() {
         return id;
